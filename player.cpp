9c546d8ac24b7095646afb59a735d33787b9d6ce
chyu0818 <chyu@caltech.edu>
2018-03-03 21:18:58 -0800

    dsfs

--- player.cpp
+++ player.cpp
@@ -1,6 +1,7 @@
 #include "player.hpp"
  
 /*
+ *
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
