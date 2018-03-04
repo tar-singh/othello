a44f7be9e3af8726bc00456bb8b864aa0818974d
chyu0818 <chyu@caltech.edu>
2018-03-04 09:13:54 -0800

    player.cpp

--- player.cpp
+++ player.cpp
@@ -1,5 +1,6 @@
 #include "player.hpp"
 
+
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
